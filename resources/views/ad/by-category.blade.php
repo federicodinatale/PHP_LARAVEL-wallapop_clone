5ce1edaf76114775e131c85b9db5ee860cf322c2
Federico Di Natale <federico@MacBook-Air-de-Federico.local>
2022-09-03 17:30:22 +0200

    end user2

--- resources/views/ad/by-category.blade.php
+++ resources/views/ad/by-category.blade.php
@@ -4,17 +4,17 @@
     <h1 class=""><span class="title-underline">{{$category->name}} </span>de segunda mano</h1>
 
     <div class="row">
-        <div class="col-10 flex-grow-1">
+        <div class="col-8 flex-grow-1">
             <form class="d-flex " role="search">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search search-icon" viewBox="0 0 16 16">
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 </svg>
-                <input class="form-control me-2 search-navbar w-75" type="search" placeholder="Buscar en Todas las categorias" aria-label="Search">
+                <input class="form-control me-2 search-navbar-cat w-100" type="search" placeholder="Buscar en Todas las categorias" aria-label="Search">
               </form>
         
         </div>
-        <div class="col-2">
-            <a href="" class="btn">Prufondizar Buscada</a>
+        <div class="col-4">
+            <a href="" class="btn btn-search-cat">Prufondizar Busqueda</a>
         </div>
     </div>
     
--- resources/views/vendor/pagination/simple-bootstrap-5.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-5.blade.php
@@ -1,15 +1,15 @@
 
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="Pagination Navigation" class="position-relative mx-auto">
+    <div role="navigation" aria-label="Pagination Navigation" class="position-relative mx-auto page-nav">
         <ul class="pagination justify-content-center">
             
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled" aria-disabled="true">
-                    <span class="page-link">{!! __('pagination.previous') !!}</span>
+                    <span class="page-link bg-paginate-disabled">{!! __('pagination.previous') !!}</span>
                 </li>
             @else
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">
+                    <a class="page-link bg-paginate" href="{{ $paginator->previousPageUrl() }}" rel="prev">
                         {!! __('pagination.previous') !!}
                     </a>
                 </li>
@@ -18,13 +18,13 @@
             
             @if ($paginator->hasMorePages())
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">{!! __('pagination.next') !!}</a>
+                    <a class="page-link bg-paginate" href="{{ $paginator->nextPageUrl() }}" rel="next">{!! __('pagination.next') !!}</a>
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true">
-                    <span class="page-link">{!! __('pagination.next') !!}</span>
+                    <span class="page-link bg-paginate-disabled">{!! __('pagination.next') !!}</span>
                 </li>
             @endif
         </ul>
-    </nav>
+    </div>
 @endif 
